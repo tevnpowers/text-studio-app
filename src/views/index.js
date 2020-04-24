5c494ac68a8b7cfb4899eab90bfb422934582184
Tev'n Powers <tevnpowers@gmail.com>
2020-04-24 02:02:44 -0700

    add file which exports all views in subdirectories

---
+++ src/views/index.js
@@ -0,0 +1,7 @@
+export { default as Account } from './Account';
+export { default as Dashboard } from './Dashboard';
+export { default as DataBrowser } from './DataBrowser';
+export { default as Dataset } from './Dataset';
+export { default as NotFound } from './NotFound';
+export { default as Project } from './Project';
+export { default as Settings } from './Settings';
