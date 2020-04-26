554174a70f3b76f0872c842c2890e9da3fc94bfe
Tev'n Powers <tevnpowers@gmail.com>
2020-04-26 04:07:04 -0700

    add new views for Home page & Browse page

--- src/views/index.js
+++ src/views/index.js
@@ -1,7 +1,8 @@
 export { default as Account } from './Account';
+export { default as Browse } from './Browse';
 export { default as Dashboard } from './Dashboard';
-export { default as DataBrowser } from './DataBrowser';
 export { default as Dataset } from './Dataset';
+export { default as Home } from './Home';
 export { default as NotFound } from './NotFound';
 export { default as Project } from './Project';
 export { default as Settings } from './Settings';
